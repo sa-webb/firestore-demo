37e2a2f816276be88e4af99f36fd44c815deb84d
Austin Webb <stevenaustinwebb@gmail.com>
2019-03-09 22:07:40 -0500

    minor adjustments

--- src/config/Firestore.js
+++ src/config/Firestore.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase';
 
 var config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBtQt1-j8D8x7XAeAhtmETdF5EE4D1O9qw",
+  authDomain: "firestore-demo-51d82.firebaseapp.com",
+  databaseURL: "https://firestore-demo-51d82.firebaseio.com",
+  projectId: "firestore-demo-51d82",
+  storageBucket: "firestore-demo-51d82.appspot.com",
+  messagingSenderId: "978566309815"
+};
+firebase.initializeApp(config);
 
-  const db = firebase.firestore()
-  
-  export default db;
+const db = firebase.firestore()
+
+export default db;
